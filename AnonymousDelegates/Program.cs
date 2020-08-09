37e83faf3341cb4e4284e33a8a05cba58924deb7
marco villanueva <villanuevamarco98@gmail.com>
2020-08-09 16:43:41 -0600

    Add project files.

---
+++ AnonymousDelegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AnonymousDelegates
+{
+    public delegate string myDelegate(int arg1, int arg2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //delegate in this context is used to declare the 
+            //the function in line with the code
+            myDelegate f = delegate (int arg1, int arg2)
+            {
+                return (arg1 + arg2).ToString();
+            };
+            Console.WriteLine("The number is " + f(10, 20));
+        }
+    }
+}
